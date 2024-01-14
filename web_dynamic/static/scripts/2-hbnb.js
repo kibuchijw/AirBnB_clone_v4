27a48157c6744f43fca5ba0e3c76a717d42f8f3a
Shadkoech <shadrackipruto@gmail.com>
2024-01-14 21:42:26 +0300

    Improved on 2-hbnb.html, 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
   });
 
   // get status of API
-  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+  $.getJSON('http://127.0.0.1:5001/api/v1/status/', (data) => {
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
     } else {
